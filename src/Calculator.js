0397cdd49c82d6690ba6b9d6990b3d12a39d0266
abakirh <abakirhanna@gmail.com>
2021-02-15 12:13:03 -0500

    Caculator App So far

---
+++ src/Calculator.js
@@ -0,0 +1,30 @@
+const Calculation = require('./models/Calculation')
+const Sum = require('./Operations/Sum');
+const Difference = require('./Operations/Difference');
+const Product = require('./Operations/Product');
+const Quotient = require('./Operations/Quotient');
+
+class Calculator {
+    static Calculations = []
+    static Sum(a,b) {
+          let calculation = new Calculation(a,b,Sum);
+          Calculator.Calculations.push(calculation);
+        return calculation.GetResults();
+    }
+    static Difference(a,b) {
+        let calculation = new Calculation(a,b,Difference);
+        Calculator.Calculations.push(calculation);
+        return calculation.GetResults();
+    }
+    static Product(a,b) {
+        let calculation = new Calculation(a,b,Product);
+        Calculator.Calculations.push(calculation);
+        return calculation.GetResults();
+    }
+    static Quotient(a,b) {
+        let calculation = new Calculation(a,b,Quotient);
+        Calculator.Calculations.push(calculation);
+        return calculation.GetResults();
+    }
+}
+module.exports = Calculator;
---
+++ tests/Calculator.test.js
@@ -0,0 +1,26 @@
+const Calculator = require('../src/Calculator');
+
+test('Calculator adding two numbers', () => {
+    let result = Calculator.Sum(1,2);
+    expect(result).toBe(3);
+});
+test('Calculator difference between two numbers', () => {
+    let result = Calculator.Difference(1,2);   
+    expect(result).toBe(-1);
+});
+test('Calculator product of two numbers', () => {
+    let result = Calculator.Product(1,2);
+    expect(result).toBe(2);
+});
+test('Calculator quotient of two numbers', () => {
+    let result = Calculator.Quotient(1,2);
+    expect(result).toBe(.5);
+});
+test('Calculator adding to calculations', () => {
+    let result = Calculator.Quotient(1,2);
+    let calculations = Calculator.Calculations
+    calculations.forEach(function (calc) {
+       console.log(calc.GetResults())
+    });
+});
+
