12c7af2cd08e703eea0c25d3be9eb92fcb8a7604
abakirh <abakirhanna@gmail.com>
2021-02-17 06:50:38 -0500

    renamed a folder name

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-const Calculation = require('./models/Calculation')
+const Calculation = require('./Models/Calculation')
 const Sum = require('./Operations/Sum');
 const Difference = require('./Operations/Difference');
 const Product = require('./Operations/Product');
