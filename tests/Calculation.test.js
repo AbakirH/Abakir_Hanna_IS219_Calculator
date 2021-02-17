2c9ad17890a3ce0db6b091236b7211d54cab6310
abakirh <abakirhanna@gmail.com>
2021-02-17 07:13:06 -0500

    fixed some coments

--- tests/Calculation.test.js
+++ tests/Calculation.test.js
@@ -15,49 +15,49 @@ test('Test of Calculation Instantiation', () => {
     expect(calculation.op).toBe(op);
 });
 test('Test Get results for Sum function', () => {
-    //Test the get results function
+    //Test the sum function
     let op = Sum;
     let calculation = new Calculation(1,2,op);
     expect(calculation.GetResults()).toBe(3);
 });
 test('Test Get results for Difference function', () => {
-    //Test the get results function
+    //Test the difference function
     let op = Difference;
     let calculation = new Calculation(1,2,op);
     expect(calculation.GetResults()).toBe(-1);
 });
 test('Test Get results for Product function', () => {
-    //Test the get results function
+    //Test the product function
     let op = Product;
     let calculation = new Calculation(1,2,op);
     expect(calculation.GetResults()).toBe(2);
 });
 test('Test Get results for Quotient function', () => {
-    //Test the get results function
+    //Test the quotient function
     let op = Quotient;
     let calculation = new Calculation(1,2,op);
     expect(calculation.GetResults()).toBe(.5);
 });
 test('Test Get results for Power function', () => {
-    //Test the get results function
+    //Test the power function
     let op = Power;
     let calculation = new Calculation(3,2,op);
     expect(calculation.GetResults()).toBe(9);
 });
 test('Test Get results for Root function', () => {
-    //Test the get results function
+    //Test the root function
     let op = Root;
     let calculation = new Calculation(4,2,op);
     expect(calculation.GetResults()).toBe(2);
 });
 test('Test Get results for Square function', () => {
-    //Test the get results function
+    //Test the square function
     let op = Power;
     let calculation = new Calculation(3,2,op);
     expect(calculation.GetResults()).toBe(9);
 });
 test('Test Get results for Root function', () => {
-    //Test the get results function
+    //Test the root function
     let op = Root;
     let calculation = new Calculation(4,2,op);
     expect(calculation.GetResults()).toBe(2);
